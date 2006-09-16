67b44f482c67fd339bee4d6891529b3983e60dd6
chris.chiasson <chris@chiasson.name>
2006-09-16 19:17:33 +0000

    added ant logging of the executing package path, the executing mfiles, and the failed mfiles.

--- XML/DocBook/MMADEant.m
+++ XML/DocBook/MMADEant.m
@@ -2,11 +2,11 @@
 PrependTo[$Path,
 	AntProperty["env.MMADE_XML_PARENT_DIR"]];
 
-AntLog[$Path];
-
 <<"XML`DocBook`";
 
 $Path=Join[Ant["Project"]@getReference["mpath"]@list[],$Path];
+
+AntLog[$Path];
 
 $DisplayFunction=Identity;
 
@@ -14,8 +14,10 @@ Module[{failedmFiles,
 	mFiles=Ant["Project"]@getReference["mfiles"]@list[],
 	stringTrueQ=StringMatchQ[ToString@#,"True",IgnoreCase->True]&},
 	failedmFiles=Pick[#,FileType/@#,None|Directory]&[mFiles];
+       AntLog[$mFiles];
+       AntLog[$Path];
 	If[failedmFiles=!={},
-		"The following mfile(s) that you wanted me"<>
+		"The following mfile(s) that you wanted me "<>
 			"to execute do(es) not exist: "<>
 			ToString@failedmFiles];
 	If[stringTrueQ@AntProperty["usexvnc"],
