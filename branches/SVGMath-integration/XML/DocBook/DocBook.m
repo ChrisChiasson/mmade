7ca0b8f33ebda1724a02429f5a18de246b399f5d
chris.chiasson <chris.chiasson@5fbf8a15-391d-0410-9829-7f8e261965da>
2006-11-16 10:25:28 +0000

    added a Ghostscript overload of the "PDF" Export and defaulted the FO output type to PDF

--- branches/SVGMath-integration/XML/DocBook/DocBook.m
+++ branches/SVGMath-integration/XML/DocBook/DocBook.m
@@ -1180,7 +1180,7 @@ $pngHtmlExpressionExportOptions={
 $epsPdfExpressionExportOptions={
 	ConversionOptions->{"IncludeSpecialFonts"->True},
 	DataAttributes->{(*pdfScaleAttribute*)},
-	ExportType->"EPS",
+	ExportType->"PDF",
 	(*ImageResolution:>$PrintResolution,*)
 	ObjectAttributes->{"role"->"fo"}
 	};
@@ -1658,6 +1658,65 @@ ExportDryRun[file_,expr_,type_String,opts:optionsOrNullPseudoPatternObject]:=
 	{Message[Export::chtype,file];file,ExportString[expr,type,opts]};
 
 defineBadArgs@ExportDryRun;
+
+(*ghostscript*)
+
+Ghostscript`Executable::notfound="The XML`DocBook` package can't find \
+Ghostscript. The package needs Ghostscript to overload the Export function for \
+PDFs. Please set Ghostscript`Executable equal to the path of your copy of \
+Ghostscript. On Windows machines, this is likely to be the path to \
+gswin32c.exe. On Linux machines, this is likely to be the path to gs. You may \
+freely download a copy of Ghostscript from http://www.cs.wisc.edu/~ghost/. If \
+you don't want to see this message every time you load the XML`DocBook` \
+package, put the definition of Ghostscript`Executable in your kernel's init.m \
+file or set the ghostscript property in one of your MMADE Ant configuration \
+files.";
+
+If[!ValueQ@Ghostscript`Executable,
+	Ghostscript`Executable=AntProperty["ghostscript"]];
+
+If[!StringQ[Ghostscript`Executable],
+	Ghostscript`Executable=
+		Switch[$SystemID,
+			"Windows",
+			"C:\\Program Files\\gs\\gs8.54\\bin\\gswin32c.exe",
+			"Linux",
+			"/usr/bin/gs"
+			]
+	];
+
+If[!StringQ@Ghostscript`Executable||FileType@Ghostscript`Executable===None,
+  Message[Ghostscript`Executable::notfound]
+  ];
+
+Unprotect[System`ConvertersDump`exportFormatQ,Message,Export];
+Update/@{System`ConvertersDump`exportFormatQ,Message,Export};
+
+System`ConvertersDump`exportFormatQ["PDF"]=False;
+
+Message[Export::format,_]=Sequence[];
+
+Export[pdfFile_String,xpr_,"PDF",opts___?OptionQ]/;
+	StringQ@Ghostscript`Executable&&FileType@Ghostscript`Executable===File:=
+	Module[{args,epsFile,stem},
+		epsFile=StringReplace[pdfFile,stem__~~".pdf"\[Rule]stem~~".eps"];
+		Export[epsFile,xpr,"EPS",opts];
+		If[0===
+			Run["\""<>Ghostscript`Executable<>"\"",
+				"-dCompatibilityLevel=1.4","-q","-dSAFER","-dNOPAUSE","-dBATCH",
+				"-sDEVICE=pdfwrite","-sOutputFile="<>pdfFile,"-c",
+				".setpdfwrite","-f",fullPathNameExport[epsFile,"EPS"]
+				],
+			pdfFile,
+			$Failed
+			]
+		];
+
+Export[pdfFile_String,xpr_,"PDF",opts___?OptionQ]:=
+	Message[Ghostscript`Executable::notfound];
+
+Protect[System`ConvertersDump`exportFormatQ,Message,Export];
+Update/@{System`ConvertersDump`exportFormatQ,Message,Export};
 
 End[];
 EndPackage[];
