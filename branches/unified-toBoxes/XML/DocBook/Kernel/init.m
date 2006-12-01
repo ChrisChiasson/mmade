06257ab7a47f5856b942632d603b5188ed3e01a1
chris.chiasson <chris.chiasson@5fbf8a15-391d-0410-9829-7f8e261965da>
2006-12-01 09:41:18 +0000

    these branches have been superseded by the trunk (actually, PDF-only-for-FO was the best version, and I copied it over to the trunk)

--- branches/unified-toBoxes/XML/DocBook/Kernel/init.m
+++
@@ -1,8 +0,0 @@
-(* Mathematica Init File *)
-
-Get["XML`DocBook`DocBook`"]
-(*
-MMADE, a Mathematica DocBook Exporter
-The license and Copyright information for MMADE is included in rights.txt
-in the XML directory.
-*)
