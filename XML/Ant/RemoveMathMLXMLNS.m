7c80800c77467a4b0fb5ed810de6832f227b3ce6
chris.chiasson <chris@chiasson.name>
2007-01-28 01:50:52 +0000

    added RemoveMathMLXMLNS script to remove the extraneous xmlns attribute pointing to the MathML namespace url that SVGMath leaves on inline SVGs

---
+++ XML/Ant/RemoveMathMLXMLNS.m
@@ -0,0 +1,25 @@
+(*this file is called from the SVGMath related task of build.xml*)
+removeMathMLXMLNSFiles=
+	Ant["Project"]@
+		getReference["removemathmlxmlnsfiles"]@
+			list[]
+
+Export[#,
+	ReplacePart[
+		Import[#,"SymbolicXML",
+			ConversionOptions->{"NormalizeWhitespace"->False}
+			]/.
+			HoldPattern[{"http://www.w3.org/2000/xmlns/","xmlns"}->
+				"http://www.w3.org/1998/Math/MathML"
+				]->Sequence[],
+		"Encoding"->"UTF-8",
+		{1,1,-1}
+		],
+	"XML",
+	ConversionOptions->{"ElementFormatting"->None}
+	]&/@removeMathMLXMLNSFiles
+(*
+MMADE, a Mathematica DocBook Exporter
+The license and Copyright information for MMADE is included in rights.txt
+in the XML directory.
+*)
