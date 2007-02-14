5fbc2a687a3a6bc15cd2d11c8be44d3c4446c0ec
chris.chiasson <chris.chiasson@5fbf8a15-391d-0410-9829-7f8e261965da>
2007-02-14 18:37:19 +0000

    changed the init file to load my MathML Workarounds package

--- trunk/XML/MathML/Workarounds/Kernel/init.m
+++ trunk/XML/MathML/Workarounds/Kernel/init.m
@@ -1,6 +1,6 @@
 (* Mathematica Init File *)
 
-Get["XML`DocBook`DocBook`"]
+Get["XML`MathML`Workarounds`Workarounds`"]
 (*
 MMADE, a Mathematica DocBook Exporter
 The license and Copyright information for MMADE is included in rights.txt
