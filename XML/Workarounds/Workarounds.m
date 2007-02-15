035a1d9842cc91bbe0bd457cb40dbb5355f4b7fa
chris.chiasson <chris@chiasson.name>
2007-02-15 12:18:44 +0000

    imported the rawXML function from DocBook.m

--- XML/Workarounds/Workarounds.m
+++ XML/Workarounds/Workarounds.m
@@ -4,6 +4,17 @@ BeginPackage["XML`Workarounds`"]
 
 Begin["`Private`"]
 
+rawXML[mathMl_String,opts:optionsOrNullPseudoPatternObject]:=
+	Sequence@@
+		ImportString[
+			"<llamabait>"<>mathMl<>"</llamabait>",
+			xmlFileType,
+			ConversionOptions->{"NormalizeWhitespace"->False},
+			FilterOptions[ImportString,opts]
+			][[2,3]];
+
+GeneralDownValue@rawXML;
+
 
 End[]
 
