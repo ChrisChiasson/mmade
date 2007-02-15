5179137970a9d56ffcdfb03ebba89bd05d29c69b
chris.chiasson <chris@chiasson.name>
2007-02-15 03:40:52 +0000

    mtext and ms modification functions from DocBook.m

--- XML/MathML/Workarounds/Workarounds.m
+++ XML/MathML/Workarounds/Workarounds.m
@@ -1,12 +1,125 @@
+(* ::Package:: *)
+
 BeginPackage["XML`MathML`Workarounds`"]
 
 
 Begin["`Private`"]
 
+(*context handling*)
+If[!NameQ[#<>"*"],Get@#]&/@{"Utilities`BadArgumentHandling`"}
 
 old$ContextPath=$ContextPath
-$ContextPath=Fold[Insert[##,2]&,$ContextPath,Reverse@{"XML`MathML`","XML`"}]
+$ContextPath=Flatten@{$ContextPath,"XML`","XML`MathML`",
+	"Utilities`BadArgumentHandling`"}
 
+(*questionable old workarounds
+
+$mspaceWidth="5pt";
+
+reformatMtext[(*\[COMPATIBILITYNoBreak]*)
+	XMLElement[mtextHead:containsMtextPatternObject,{attributes___},{"\:f3a2"}]
+	]=(*XMLElement[mtextHead,{attributes},{""}]*)Sequence[]
+
+reformatMtext[
+	XMLElement[
+		mtextHead:containsMtextPatternObject,{attributes___},{str_String}
+		]
+	]/;AtomQ[Unevaluated[str]]&&StringLength[str]>=1&&
+		StringMatchQ[StringTake[str,-1],Whitespace]:=
+	Sequence[
+		reformatMtext[XMLElement[mtextHead,{attributes},{StringDrop[str,-1]}]](*,
+		XMLElement[
+			mtextHead/."mtext"->"mspace",
+			{"width"->$mspaceWidth,attributes},
+			{}
+			]*)
+		];
+
+reformatMtext[
+	XMLElement[
+		mtextHead:containsMtextPatternObject,{attributes___},{str_String}
+		]
+	]/;AtomQ[Unevaluated[str]]&&StringLength[str]>=1&&
+		StringMatchQ[StringTake[str,1],Whitespace]:=
+	Sequence[
+		(*XMLElement[
+			mtextHead/."mtext"->"mspace",
+			{"width"->$mspaceWidth,attributes},
+			{}
+			],*)
+		reformatMtext[XMLElement[mtextHead,{attributes},{StringDrop[str,1]}]]
+		];
+
+reformatMtext[element:XMLElement[containsMtextPatternObject,{___},{___String}]]=
+	element;
+
+GeneralDownValue@reformatMtext;
+
+reformatMs[
+	XMLElement[msHead:containsMsPatternObject,{attributes___},{str_String}]/;
+		AtomQ[Unevaluated[str]]&&(!ShowStringCharacters/.
+			AbsoluteOptions[$FrontEnd,ShowStringCharacters])
+	]:=
+	Module[{midStr,strMod=StringReplace[str,{constantStringReplacements}]},
+		If[StringMatchQ[str,StringExpression["\\\"",midStr__,"\\\""]],
+			XMLElement[msHead/."ms"->"ms",
+				{attributes},
+				{StringTake[strMod,{3,-3}]}
+				],
+			XMLElement[msHead/."ms"->"mtext",{attributes},{strMod}]
+			]
+		];
+
+(*do not remove the condition from this function evaluation it is part of a 
+two by two decision matrix - you will need to look at all four conditions
+to be sure you can remove it*)
+reformatMs[XMLElement[msHead:containsMsPatternObject,{attributes___},{}]/;
+		(!ShowStringCharacters/.
+			AbsoluteOptions[$FrontEnd,ShowStringCharacters])]:=
+		XMLElement[msHead/."ms"->"mtext",{attributes},{}];
+
+reformatMs[elem:XMLElement[containsMsPatternObject,__]]:=elem;
+
+GeneralDownValue@reformatMs;
+
+end of questionable old workarounds*)
+
+
+
+formatNumberFormMathMLNumber[str_String]:=
+	Module[
+		{number},
+		(*this should be a regular ToBoxes call, not toBoxes*)
+		ToBoxes@number/;
+			validateGiveNumber[ToExpression[str,InputForm,HoldComplete],number]
+		];
+
+GeneralDownValue@formatNumberFormMathMLNumber;
+
+
+formatNumberFormMathMLInterpretationBox[
+	boxes_,(number:_Real|_Integer)?NumberQ,otherArgs___
+	]:=
+	Module[{str},boxes/.str_String/;AtomQ[Unevaluated[str]]:>
+										formatNumberFormMathMLNumber[str]];
+
+GeneralDownValue@formatNumberFormMathMLInterpretationBox;
+
+
+formatNumberFormMathMLBoxes[boxes_]:=
+	Module[{intBoxes,number,otherArgs,result},
+		boxes/.InterpretationBox[intBoxes_,number_?NumberQ,otherArgs___]:>
+			Block[
+				{InterpretationBox},
+				formatNumberFormMathMLInterpretationBox[
+					intBoxes,
+					number,
+					otherArgs
+					]/;True
+				]
+		];
+
+GeneralDownValue@formatNumberFormMathMLBoxes;
 
 
 $ContextPath=old$ContextPath
