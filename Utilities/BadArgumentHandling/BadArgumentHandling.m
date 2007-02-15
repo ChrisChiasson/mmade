5bbb069a6c4930a9844b50286b8f00288c69b773
chris.chiasson <chris@chiasson.name>
2007-02-15 00:16:01 +0000

    added the BadArgumentHandling package file template

---
+++ Utilities/BadArgumentHandling/BadArgumentHandling.m
@@ -0,0 +1,21 @@
+(* ::Package:: *)
+
+BeginPackage["Utilities`BadArgumentHandling`"]
+
+
+Begin["`Private`"]
+
+
+
+
+End[]
+
+
+EndPackage[]
+
+
+(*
+MMADE, a Mathematica DocBook Exporter
+The license and Copyright information for MMADE is included in rights.txt
+in the XML directory.
+*)
