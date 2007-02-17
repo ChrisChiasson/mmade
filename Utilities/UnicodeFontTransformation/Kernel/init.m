7242b48bb9a92771f66dfdc8448e167bab66f34b
chris.chiasson <chris@chiasson.name>
2007-02-17 06:59:26 +0000

    copied Kernel folder from BadArgumentHandling to UnicodeFontTransformation for use as a template

---
+++ Utilities/UnicodeFontTransformation/Kernel/init.m
@@ -0,0 +1,8 @@
+(* Mathematica Init File *)
+
+Get["Utilities`BadArgumentHandling`BadArgumentHandling`"]
+(*
+MMADE, a Mathematica DocBook Exporter
+The license and Copyright information for MMADE is included in rights.txt
+in the XML directory.
+*)
