a6acd824809d661058ad0c29ec032d90402a98e8
chris.chiasson <chris@chiasson.name>
2007-02-21 16:25:04 +0000

    changed template to point to Utilities FileHandling FileHandling

--- Utilities/FileHandling/Kernel/init.m
+++ Utilities/FileHandling/Kernel/init.m
@@ -1,6 +1,6 @@
 (* Mathematica Init File *)
 
-Get["Utilities`FilterOptions`Extensions`Extensions`"]
+Get["Utilities`FileHandling`FileHandling`"]
 (*
 MMADE, a Mathematica DocBook Exporter
 The license and Copyright information for MMADE is included in rights.txt
