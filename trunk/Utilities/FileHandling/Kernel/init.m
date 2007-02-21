58992e38d83224924403e288a02c63bee8a23d29
chris.chiasson <chris.chiasson@5fbf8a15-391d-0410-9829-7f8e261965da>
2007-02-21 16:23:56 +0000

    copied Utilities FilterOptions Extensions Kernel folder to Utilities FileHandling as a template

---
+++ trunk/Utilities/FileHandling/Kernel/init.m
@@ -0,0 +1,8 @@
+(* Mathematica Init File *)
+
+Get["Utilities`FilterOptions`Extensions`Extensions`"]
+(*
+MMADE, a Mathematica DocBook Exporter
+The license and Copyright information for MMADE is included in rights.txt
+in the XML directory.
+*)
